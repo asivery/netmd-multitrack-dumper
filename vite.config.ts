52e2a9aab4aa4c16d6defa67ef0dfae690c91e54
asivery <asivery@protonmail.com>
2025-05-05 20:15:24 +0200

    Fix polyfill buffer

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import react from '@vitejs/plugin-react'
+import inject from '@rollup/plugin-inject';
 let base = process.env.PUBLIC_URL ?? '/';
 if(!base.endsWith("/")) base += '/';
 
 console.log(`Building for base = ${base}`);
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base,
-  plugins: [react()],
-})
+export default ({ mode }: { mode: any }) => {
+  return defineConfig({
+    base,
+    plugins: [
+      react(),
+      nodePolyfills({
+        globals: {
+          Buffer: false,
+        },
+        exclude: ['buffer'],
+      }),
+    ],
+    build: {
+      commonjsOptions: { transformMixedEsModules: true },
+      rollupOptions: {
+        plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+      },
+    }
+  })
+}
